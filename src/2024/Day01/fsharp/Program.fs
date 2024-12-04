7ea4990ea223015c8ff456b0f430492d4bd23f2f
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-12-04 16:37:21 -0600

    WIP

--- src/2024/Day01/fsharp/Program.fs
+++ src/2024/Day01/fsharp/Program.fs
@@ -1,7 +1,18 @@
 ﻿module Day01
 
+open System
 open System.IO
+
+let handle lines =
+    lines
+    |> Seq.choose (fun (x: string) ->
+        match Int32.TryParse(x) with
+        | true, i -> Some i
+        | _ -> None)
+
+let p1 lines =
+    lines |> Seq.map (fun (x: string) -> x.Split(" ") |> handle)
 
 let input = File.ReadLines "input"
 
-printfn $"{input}"
+printfn $"{p1 input}"

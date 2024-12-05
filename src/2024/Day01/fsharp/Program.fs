89036fbce739288c097f6b4969493dbbfb082c9b
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-12-04 20:32:38 -0600

    P1 and WIP on P2

--- src/2024/Day01/fsharp/Program.fs
+++ src/2024/Day01/fsharp/Program.fs
@@ -3,16 +3,40 @@
 open System
 open System.IO
 
-let handle lines =
+let numbers lines =
     lines
     |> Seq.choose (fun (x: string) ->
         match Int32.TryParse(x) with
         | true, i -> Some i
         | _ -> None)
+    |> Seq.toList
 
 let p1 lines =
-    lines |> Seq.map (fun (x: string) -> x.Split(" ") |> handle)
+    lines
+    |> Seq.map (fun (x: string) -> x.Split(" ") |> numbers)
+    |> List.transpose
+    |> List.map List.sort
+    |> List.transpose
+    |> List.map (function
+        | [ a; b ] -> a - b
+        | _ -> failwith "input")
+    |> List.map Math.Abs
+    |> List.fold (+) 0
+
+let p2 lines =
+    lines
+    |> Seq.map (fun (x: string) -> x.Split(" ") |> numbers)
+    |> List.transpose
+    |> List.map (
+        List.sort
+        >> List.fold
+            (fun s x ->
+                s
+                |> Map.change x (Option.defaultValue 0 |> Option.map (+ 1))
+            Map.empty
+    )
 
 let input = File.ReadLines "input"
 
 printfn $"{p1 input}"
+printfn $"{p2 input}"

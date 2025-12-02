f309c209d84348e22cb597308c026148d508877d
UnstoppableMango <erik.rasmussen@unmango.dev>
2025-12-02 00:40:17 -0600

    wip

--- src/2025/Day01/ocaml/bin/main.ml
+++ src/2025/Day01/ocaml/bin/main.ml
@@ -1,10 +1,15 @@
+(* https://gist.github.com/Chubek/524bcc10abc593710f7de9ccca8377e4?permalink_comment_id=5073295#gistcomment-5073295 *)
+let explode s = List.of_seq (String.to_seq s)
+let implode l = String.of_seq (List.to_seq l)
+
 let read_lines (file_name : string) : string list =
   In_channel.with_open_text file_name In_channel.input_lines
 
-let split s =
-  (String.sub s 0 1, String.sub s 1 (String.length s - 1) |> int_of_string)
-
-let conv (d, n) = match d with "R" -> n | "L" -> -n | _ -> failwith "heck"
+let conv s =
+  match explode s with
+  | 'R' :: t -> int_of_string (implode t)
+  | 'L' :: t -> -int_of_string (implode t)
+  | _ -> failwith "heck"
 
 let rec calc acc x =
   match acc + x with
@@ -13,21 +18,36 @@ let rec calc acc x =
   | n -> n
 
 let p1 () =
-  read_lines "input.txt" |> List.map split |> List.map conv |> List.to_seq
-  |> Seq.scan calc 50
+  read_lines "input.txt" |> List.map conv |> List.to_seq |> Seq.scan calc 50
   |> Seq.filter (fun x -> x = 0)
   |> Seq.length |> string_of_int |> print_endline
 
-let c1 = function
-  | n when n mod 100 = 0 -> 1
-  | n ->
-      let () = Printf.printf "%d / 100 = %d\n" n (n / 100) in
-      n / 100
+(* let rec count dial c =
+  let () = Printf.printf "              %4d %2d\n" dial c in
+  if dial < 0 then count (dial + 100) (c + 1)
+  else if dial = 100 then (0, c)
+  else if dial > 99 then count (dial - 100) (c + 1)
+  else (dial, c)
+
+let calc2 (dial, c) turns =
+  let () = Printf.printf "%4d + %4d = %4d\n" dial turns (dial + turns) in
+  match count (dial + turns) c with
+  | (0, c) -> 0, c + 1
+  | x -> x *)
+
+let calc3 (dial, c) turns =
+ let () = Printf.printf "%4d + %4d = %4d (%d)\n" dial turns (dial + turns mod 100) c in
+ match (dial + turns mod 100) with
+ | dial' when dial' < 0 -> dial' + 100, 1 + c + (turns / 100)
+ | 100 -> 0, c
+ | dial' when dial' > 99 -> dial' - 100, c + (turns / 100)
+ | 0 -> 0, c + 1
+ | x -> x, c
 
 let p2 () =
-  read_lines "input.txt" |> List.map split |> List.map conv |> List.to_seq
-  |> Seq.scan ( + ) 50 |> Seq.map c1 |> Seq.fold_left ( + ) 0 |> string_of_int
-  |> print_endline
+  read_lines "input.txt" |> List.map conv |> List.to_seq
+  |> Seq.fold_left calc3 (50, 0)
+  |> snd |> string_of_int |> print_endline
 
 let () =
   let () = p1 () in

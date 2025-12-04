a316b34ab2988885dfb3e9e1df556b238e929554
UnstoppableMango <erik.rasmussen@unmango.dev>
2025-12-03 20:29:57 -0600

    day3 p1

--- src/2025/Day03/ocaml/bin/main.ml
+++ src/2025/Day03/ocaml/bin/main.ml
@@ -1,15 +1,23 @@
-let batteries s =
-  String.to_seq s |> Seq.map int_of_char
+let batteries s = String.to_seq s
 
 let best bank =
-  List.sort compare bank |> function | a :: b :: _ -> a + b | _ -> failwith "heck"
+  let () = Printf.printf "bank: %s\n" (String.of_seq bank) in
+  bank
+  |> Seq.mapi (fun i a ->
+      bank
+      |> Seq.drop (i + 1)
+      |> Seq.map (fun b ->
+          (* let () =
+            Printf.printf "%s; " ([ a; b ] |> List.to_seq |> String.of_seq)
+          in *)
+          [ a; b ] |> List.to_seq |> String.of_seq |> int_of_string))
+  |> Seq.flat_map Fun.id |> Seq.fold_left Int.max 0
 
 let read_input name =
   In_channel.with_open_text name In_channel.input_lines
-  |> List.map batteries
-  |> List.map best
-  |> List.fold_left (+) 0
-  |> string_of_int
-  |> print_endline
+  |> List.map batteries |> List.map best
+  |> fun x ->
+  let () = x |> List.iter (Printf.printf "%d\n") in
+  x |> List.fold_left ( + ) 0
 
-let () = read_input "sample"
+let () = read_input "input" |> string_of_int |> print_endline
